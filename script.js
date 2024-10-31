d2686e21614a5d009ae0dd87c1db26715b742f8a
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-10-31 16:34:21 +0000

    math.floor

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 
 
-let question = prompt("Whats's my number");
-let correct = "Thats correct";
-let wrong = "Thats wrong";
-let myNum = 31;
+// let question = prompt("Whats's my number");
+// let correct = "Thats correct";
+// let wrong = "Thats wrong";
+// // let myNum = 31;
 
-function check(){
-    if (question == myNum){
-        console.log(correct + " " + myNum + " " + "was my number")
-    }else if (question < myNum){
-        console.log("Thats too low")
-    } else if (question > myNum){
-        console.log("Thats too high")
-    }
-    else {
-        console.log(wrong + " " + "Only numbers please") 
-    }
+// function check(myNum){
+//     if (question == myNum){
+//         console.log(correct + " " + myNum + " " + "was my number")
+//     }else if (question < myNum){
+//         console.log("Thats too low")
+//     } else if (question > myNum){
+//         console.log("Thats too high")
+//     }
+//     else {
+//         console.log(wrong + " " + "Only numbers please") 
+//     }
+// }
+// check(343)
+
+let 
+
+function getMilk(money){
+    let numberOfBottles = money / 1.5;
+    console.log("Buy" + " " + Math.floor(numberOfBottles) + " " + "Botteles of milk")
 }
-check()
+getMilk(5)
