eeba89db18edeeb0bd5a7561262353c1e383f2b0
Jose Parra <110458931+JoseParra28@users.noreply.github.com>
2024-11-01 15:06:45 +0000

    practice

--- script.js
+++ script.js
@@ -41,27 +41,56 @@
 // }
 // buyFlowers(19.99)
 
-let question = prompt("Whats my name")
+// let question = prompt("Whats my name")
 
-const myList = ["Apple", "banana", "strawberry", "pear"]
+// const myList = ["Apple", "banana", "strawberry", "pear"]
 
-function answer(value){
-    if (question == value){
-        document.write("<h1>" + question + " Well done" + "</h1>")
-    }else if (question < value){
-        document.write("<li>" + myList[0] + "</li>",
-                       "<li>" + myList[2] + "</li>"
-        )
-    }else if (question > value){
-        myList.push("Lemon")
-        document.write("<h1>" + myList[4] + "</h1>")
-    }else {
-        document.write("<h1>" + "Not a number" + "</h1>")
-    }
+// function answer(value){
+//     if (question == value){
+//         document.write("<h1>" + question + " Well done" + "</h1>")
+//     }else if (question < value){
+//         document.write("<li>" + myList[0] + "</li>",
+//                        "<li>" + myList[2] + "</li>"
+//         )
+//     }else if (question > value){
+//         myList.push("Lemon")
+//         document.write("<h1>" + myList[4] + "</h1>")
+//     }else {
+//         document.write("<h1>" + "Not a number" + "</h1>")
+//     }
 
-}
-answer(60)
+// }
+// answer(60)
 
+// let question = prompt("How much money do you have?")
 
+// function shop(item){
+//     let buyFruit = Math.floor(question / item )
+//         window.write("<h1>" + "You can buy " + buyFruit + " items" + "</h1>")
+//     if (question < item){
+//         console.log("Sorry you dont have enough money")
+//     } else if (question >= item * 4 ){
+//         console.log("Wow you can buy so may items " + buyFruit + " in total")
+//     }else {
+//         return buyFruit
+//     }
+// }
+// shop(5)
+
+// let money = prompt("How much money do u have?")
+
+// function buyFlowers(item){
+//     let flower = Math.floor(money / item)
+//     console.log("You can buy" + " " + flower + " " + "flowers" )
+//     if (money <= item){
+//         console.log("Sorry you dont't have enough money")
+//     }else if (money >= item * 4){
+//         console.log("wow! you can buy so many flowers!!" + flower)
+//     }else {
+//         return flower 
+//     }
+    
+// }
+// buyFlowers(19.99)
 
 
