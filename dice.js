b933c2f09cb8c5f41c7ce057c4552a1a3a0ee1cb
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-11-07 17:42:06 +0000

    practice

--- dice.js
+++ dice.js
@@ -12,9 +12,26 @@ const image2 = document.querySelectorAll("img")[1];
 image1.setAttribute("src", randomImgSource)
 image2.setAttribute("src", randomImgSource)
 
- console.log(randomNumber)
- console.log(randomNumber2)
-
-const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random(1) * 6) + 2;
 const randomImgSource2 = "media/dice" + randomNumber2 + ".png"
-document.querySelectorAll("img")[1].setAttribute.randomImgSource2
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSource2);
+
+
+
+if ( randomNumber > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+   
+}else if (randomNumber2 > randomNumber){
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+
+}else {
+    document.querySelector("h1").innerHTML = "It's a tie";
+}
+
+
+const randomNames = ["Maria", "Nicolas", "Antonio", "Teresa"]
+const magiNumber = Math.floor(Math.random()*randomNames.length)
+const home = document.querySelector(".randomnumber").innerHTML = magiNumber
+
+console.log(magiNumber)
+
