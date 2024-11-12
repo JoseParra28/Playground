0515ad075a509dbceffc73c4c24aea0e1e150640
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-11-12 19:09:21 +0000

    js practice

--- dice.js
+++ dice.js
@@ -77,5 +77,16 @@ reStart.addEventListener("click", function(){
 
 })
    
+// BTN loops
 
 
+const modal = document.querySelector(".show-modal")
+const overlay = document.querySelector(".overlay")
+const btnCloseModal = document.querySelector(".close-modal")
+const btnsOpenModal = document.querySelectorAll(".show-modal")
+
+console.log(btnsOpenModal)
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', function(){
+        console.log(btnsOpenModal[i])
+    });
