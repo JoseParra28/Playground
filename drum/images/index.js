e1366195dcbdd617d2b4ff04881abf95613a0d8c
liftandcook <110458931+JoseParra28@users.noreply.github.com>
2024-11-14 17:44:31 +0000

    js practice

--- drum/images/index.js
+++ drum/images/index.js
@@ -1,1 +1,8 @@
-console.log("hey")
+let allDrums = document.querySelectorAll(".drum");
+
+for(let i =0; i < allDrums.length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        console.log(`You have checled ${i}`)
+    })
+}
+
